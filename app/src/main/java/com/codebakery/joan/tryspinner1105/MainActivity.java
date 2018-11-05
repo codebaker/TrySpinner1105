829926b5b0ccd7787ae165eaff0dc1cfb89c3201
codebaker <jungah.kim76@gmail.com>
2018-11-05 15:11:13 +0900

    Initial commit

---
+++ app/src/main/java/com/codebakery/joan/tryspinner1105/MainActivity.java
@@ -0,0 +1,44 @@
+package com.codebakery.joan.tryspinner1105;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
+        List<String> list = new ArrayList<String>();
+        list.add("Apple");
+        list.add("Mango");
+        list.add("Banana");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(dataAdapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String selected =((TextView)view).getText().toString();
+        String text = parent.getItemAtPosition(position).toString();
+
+        Toast.makeText(this,"text = " + text + "/ selected = " + selected,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
+}
